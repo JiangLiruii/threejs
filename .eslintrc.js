a1d5cc690928cf25c19292dfeb702a2a695a49d9
JiangLirui <535904200@qq.com>
2019-10-27 09:10:16 +0800

    add project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": "google",
     "parserOptions": {
         "ecmaVersion": 2016,
         "sourceType": "module"
@@ -13,7 +13,7 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            4
+            2
         ],
         "linebreak-style": [
             "error",
@@ -26,6 +26,17 @@ module.exports = {
         "semi": [
             "error",
             "never"
-        ]
+        ],
+        "brace-style": ["error", "1tbs", {allowSingleLine: true}],
+        "key-spacing": [0, { "beforeColon": false, "afterColon": true }],
+        "no-spaced-func":  "error",
+        // 嵌套块深度
+        "max-depth": [0, 4],
+        //字符串最大长度
+        "max-len": [0, 80, 4],
+        //函数最多只能有3个参数
+        "max-params": [0, 3],
+        //函数名首行大写必须使用new方式调用，首行小写必须用不带new方式调用
+        "new-cap": 2,
     }
 };
