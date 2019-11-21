a8d72de9de7afa1ff49805fb6562afe52f963cc4
JiangLirui <535904200@qq.com>
2019-11-22 06:40:53 +0800

    add basic material vertex color

--- src/pageFour/index.js
+++ src/pageFour/index.js
@@ -8,12 +8,17 @@ const spotLight = new THREE.SpotLight(0xffffff, 1, 180, Math.PI / 4)
 spotLight.castShadow = true
 spotLight.position.set(-10, 20, -10)
 
-const cubeMaterial = new THREE.MeshBasicMaterial()
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  vertexColors: THREE.VertexColors,
+})
 const cubeGeometry = new THREE.BoxGeometry(5, 5, 5)
 cubeMaterial.color = new THREE.Color(0xffccdd)
 cubeMaterial.name = 'material-1'
-cubeMaterial.opacity = '0.3'
-cubeMaterial.transparent = true
+// cubeMaterial.opacity = '0.3'
+// cubeMaterial.transparent = true
+cubeMaterial.wireframe = true
+cubeMaterial.wireframeLinewidth= 2
+cubeGeometry.colors = [new THREE.Color(0x000000), new THREE.Color(0x000000), new THREE.Color(0x000000), new THREE.Color(0x000000), new THREE.Color(0x000000), new THREE.Color(0x000000), new THREE.Color(0x000000), new THREE.Color(0x000000)]
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 cube.position.set(0, 5, 0)
 
